ebe2edacd5679b937f89f49f98f9c9ff73f6db11
Brian Turek <brian.turek@gmail.com>
2013-12-15 08:11:42 -0500

    Changed SHA-512 variant constants to reference shared values in SHA-256 variants

--- src/sha_dev.js
+++ src/sha_dev.js
@@ -819,8 +819,34 @@ var SUPPORTED_ALGS = 4 | 2 | 1;
 	{
 		var a, b, c, d, e, f, g, h, T1, T2, H, numRounds, lengthPosition, i, t,
 			binaryStringInc, binaryStringMult, safeAdd_2, safeAdd_4, safeAdd_5,
-			gamma0, gamma1, sigma0, sigma1, ch, maj, Int, K, W = [],
-			appendedMessageLength, retVal;
+			gamma0, gamma1, sigma0, sigma1, ch, maj, Int, W = [],
+			appendedMessageLength, retVal,
+			K = [
+				0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
+				0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+				0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+				0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+				0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
+				0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+				0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
+				0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+				0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+				0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+				0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
+				0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+				0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
+				0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+				0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+				0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
+			],
+			H_trunc = [
+				0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
+				0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
+			],
+			H_full = [
+				0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+				0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
+			];
 
 		/* Set up the various function handles and variable for the specific
 		 * variant */
@@ -842,38 +868,14 @@ var SUPPORTED_ALGS = 4 | 2 | 1;
 			sigma1 = sigma1_32;
 			maj = maj_32;
 			ch = ch_32;
-			K = [
-					0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
-					0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
-					0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
-					0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
-					0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
-					0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
-					0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
-					0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
-					0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
-					0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
-					0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
-					0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
-					0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
-					0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
-					0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
-					0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
-				];
 
 			if ("SHA-224" === variant)
 			{
-				H = [
-						0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
-						0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
-					];
+				H = H_trunc;
 			}
 			else /* "SHA-256" === variant */
 			{
-				H = [
-						0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-						0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
-					];
+				H = H_full;
 			}
 		}
 		else if ((variant === "SHA-384" || variant === "SHA-512") &&
@@ -896,38 +898,38 @@ var SUPPORTED_ALGS = 4 | 2 | 1;
 			ch = ch_64;
 
 			K = [
-				new Int(0x428a2f98, 0xd728ae22), new Int(0x71374491, 0x23ef65cd),
-				new Int(0xb5c0fbcf, 0xec4d3b2f), new Int(0xe9b5dba5, 0x8189dbbc),
-				new Int(0x3956c25b, 0xf348b538), new Int(0x59f111f1, 0xb605d019),
-				new Int(0x923f82a4, 0xaf194f9b), new Int(0xab1c5ed5, 0xda6d8118),
-				new Int(0xd807aa98, 0xa3030242), new Int(0x12835b01, 0x45706fbe),
-				new Int(0x243185be, 0x4ee4b28c), new Int(0x550c7dc3, 0xd5ffb4e2),
-				new Int(0x72be5d74, 0xf27b896f), new Int(0x80deb1fe, 0x3b1696b1),
-				new Int(0x9bdc06a7, 0x25c71235), new Int(0xc19bf174, 0xcf692694),
-				new Int(0xe49b69c1, 0x9ef14ad2), new Int(0xefbe4786, 0x384f25e3),
-				new Int(0x0fc19dc6, 0x8b8cd5b5), new Int(0x240ca1cc, 0x77ac9c65),
-				new Int(0x2de92c6f, 0x592b0275), new Int(0x4a7484aa, 0x6ea6e483),
-				new Int(0x5cb0a9dc, 0xbd41fbd4), new Int(0x76f988da, 0x831153b5),
-				new Int(0x983e5152, 0xee66dfab), new Int(0xa831c66d, 0x2db43210),
-				new Int(0xb00327c8, 0x98fb213f), new Int(0xbf597fc7, 0xbeef0ee4),
-				new Int(0xc6e00bf3, 0x3da88fc2), new Int(0xd5a79147, 0x930aa725),
-				new Int(0x06ca6351, 0xe003826f), new Int(0x14292967, 0x0a0e6e70),
-				new Int(0x27b70a85, 0x46d22ffc), new Int(0x2e1b2138, 0x5c26c926),
-				new Int(0x4d2c6dfc, 0x5ac42aed), new Int(0x53380d13, 0x9d95b3df),
-				new Int(0x650a7354, 0x8baf63de), new Int(0x766a0abb, 0x3c77b2a8),
-				new Int(0x81c2c92e, 0x47edaee6), new Int(0x92722c85, 0x1482353b),
-				new Int(0xa2bfe8a1, 0x4cf10364), new Int(0xa81a664b, 0xbc423001),
-				new Int(0xc24b8b70, 0xd0f89791), new Int(0xc76c51a3, 0x0654be30),
-				new Int(0xd192e819, 0xd6ef5218), new Int(0xd6990624, 0x5565a910),
-				new Int(0xf40e3585, 0x5771202a), new Int(0x106aa070, 0x32bbd1b8),
-				new Int(0x19a4c116, 0xb8d2d0c8), new Int(0x1e376c08, 0x5141ab53),
-				new Int(0x2748774c, 0xdf8eeb99), new Int(0x34b0bcb5, 0xe19b48a8),
-				new Int(0x391c0cb3, 0xc5c95a63), new Int(0x4ed8aa4a, 0xe3418acb),
-				new Int(0x5b9cca4f, 0x7763e373), new Int(0x682e6ff3, 0xd6b2b8a3),
-				new Int(0x748f82ee, 0x5defb2fc), new Int(0x78a5636f, 0x43172f60),
-				new Int(0x84c87814, 0xa1f0ab72), new Int(0x8cc70208, 0x1a6439ec),
-				new Int(0x90befffa, 0x23631e28), new Int(0xa4506ceb, 0xde82bde9),
-				new Int(0xbef9a3f7, 0xb2c67915), new Int(0xc67178f2, 0xe372532b),
+				new Int(K[ 0], 0xd728ae22), new Int(K[ 1], 0x23ef65cd),
+				new Int(K[ 2], 0xec4d3b2f), new Int(K[ 3], 0x8189dbbc),
+				new Int(K[ 4], 0xf348b538), new Int(K[ 5], 0xb605d019),
+				new Int(K[ 6], 0xaf194f9b), new Int(K[ 7], 0xda6d8118),
+				new Int(K[ 8], 0xa3030242), new Int(K[ 9], 0x45706fbe),
+				new Int(K[10], 0x4ee4b28c), new Int(K[11], 0xd5ffb4e2),
+				new Int(K[12], 0xf27b896f), new Int(K[13], 0x3b1696b1),
+				new Int(K[14], 0x25c71235), new Int(K[15], 0xcf692694),
+				new Int(K[16], 0x9ef14ad2), new Int(K[17], 0x384f25e3),
+				new Int(K[18], 0x8b8cd5b5), new Int(K[19], 0x77ac9c65),
+				new Int(K[20], 0x592b0275), new Int(K[21], 0x6ea6e483),
+				new Int(K[22], 0xbd41fbd4), new Int(K[23], 0x831153b5),
+				new Int(K[24], 0xee66dfab), new Int(K[25], 0x2db43210),
+				new Int(K[26], 0x98fb213f), new Int(K[27], 0xbeef0ee4),
+				new Int(K[28], 0x3da88fc2), new Int(K[29], 0x930aa725),
+				new Int(K[30], 0xe003826f), new Int(K[31], 0x0a0e6e70),
+				new Int(K[32], 0x46d22ffc), new Int(K[33], 0x5c26c926),
+				new Int(K[34], 0x5ac42aed), new Int(K[35], 0x9d95b3df),
+				new Int(K[36], 0x8baf63de), new Int(K[37], 0x3c77b2a8),
+				new Int(K[38], 0x47edaee6), new Int(K[39], 0x1482353b),
+				new Int(K[40], 0x4cf10364), new Int(K[41], 0xbc423001),
+				new Int(K[42], 0xd0f89791), new Int(K[43], 0x0654be30),
+				new Int(K[44], 0xd6ef5218), new Int(K[45], 0x5565a910),
+				new Int(K[46], 0x5771202a), new Int(K[47], 0x32bbd1b8),
+				new Int(K[48], 0xb8d2d0c8), new Int(K[49], 0x5141ab53),
+				new Int(K[50], 0xdf8eeb99), new Int(K[51], 0xe19b48a8),
+				new Int(K[52], 0xc5c95a63), new Int(K[53], 0xe3418acb),
+				new Int(K[54], 0x7763e373), new Int(K[55], 0xd6b2b8a3),
+				new Int(K[56], 0x5defb2fc), new Int(K[57], 0x43172f60),
+				new Int(K[58], 0xa1f0ab72), new Int(K[59], 0x1a6439ec),
+				new Int(K[60], 0x23631e28), new Int(K[61], 0xde82bde9),
+				new Int(K[62], 0xb2c67915), new Int(K[63], 0xe372532b),
 				new Int(0xca273ece, 0xea26619c), new Int(0xd186b8c7, 0x21c0c207),
 				new Int(0xeada7dd6, 0xcde0eb1e), new Int(0xf57d4f7f, 0xee6ed178),
 				new Int(0x06f067aa, 0x72176fba), new Int(0x0a637dc5, 0xa2c898a6),
@@ -941,19 +943,19 @@ var SUPPORTED_ALGS = 4 | 2 | 1;
 			if ("SHA-384" === variant)
 			{
 				H = [
-					new Int(0xcbbb9d5d, 0xc1059ed8), new Int(0x0629a292a, 0x367cd507),
-					new Int(0x9159015a, 0x3070dd17), new Int(0x0152fecd8, 0xf70e5939),
-					new Int(0x67332667, 0xffc00b31), new Int(0x98eb44a87, 0x68581511),
-					new Int(0xdb0c2e0d, 0x64f98fa7), new Int(0x047b5481d, 0xbefa4fa4)
+					new Int(0xcbbb9d5d, H_trunc[0]), new Int(0x0629a292a, H_trunc[1]),
+					new Int(0x9159015a, H_trunc[2]), new Int(0x0152fecd8, H_trunc[3]),
+					new Int(0x67332667, H_trunc[4]), new Int(0x98eb44a87, H_trunc[5]),
+					new Int(0xdb0c2e0d, H_trunc[6]), new Int(0x047b5481d, H_trunc[7])
 				];
 			}
 			else /* "SHA-512" === variant */
 			{
 				H = [
-					new Int(0x6a09e667, 0xf3bcc908), new Int(0xbb67ae85, 0x84caa73b),
-					new Int(0x3c6ef372, 0xfe94f82b), new Int(0xa54ff53a, 0x5f1d36f1),
-					new Int(0x510e527f, 0xade682d1), new Int(0x9b05688c, 0x2b3e6c1f),
-					new Int(0x1f83d9ab, 0xfb41bd6b), new Int(0x5be0cd19, 0x137e2179)
+					new Int(H_full[0], 0xf3bcc908), new Int(H_full[1], 0x84caa73b),
+					new Int(H_full[2], 0xfe94f82b), new Int(H_full[3], 0x5f1d36f1),
+					new Int(H_full[4], 0xade682d1), new Int(H_full[5], 0x2b3e6c1f),
+					new Int(H_full[6], 0xfb41bd6b), new Int(H_full[7], 0x137e2179)
 				];
 			}
 		}
