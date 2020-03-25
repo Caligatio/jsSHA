1f37809295253bc03ee5453fc432c66de2bbeb1c
Brian Turek <brian.turek@gmail.com>
2020-03-25 06:28:04 +0000

    Convert Python files to Python 3

--- test/genHashRounds.py
+++ test/genHashRounds.py
@@ -1,67 +1,69 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import binascii
 import hashlib
 
 import sha3
 
+
 def main():
     hash_funcs = [
-        ('SHA-1', hashlib.sha1),
-        ('SHA-224', hashlib.sha224),
-        ('SHA-256', hashlib.sha256),
-        ('SHA-384', hashlib.sha384),
-        ('SHA-512', hashlib.sha512)]
+        ("SHA-1", hashlib.sha1),
+        ("SHA-224", hashlib.sha224),
+        ("SHA-256", hashlib.sha256),
+        ("SHA-384", hashlib.sha384),
+        ("SHA-512", hashlib.sha512),
+    ]
 
     for (sha_type, sha_func) in hash_funcs:
-        digest = sha_func('abc').digest()
+        digest = sha_func("abc".encode()).digest()
 
         # Only loop 4 times since an iteration was done above
-        for x in xrange(0, 4):
+        for x in range(0, 4):
             digest = sha_func(digest).digest()
-        print('{} with 5 Rounds: {}'.format(sha_type, digest.encode('hex')))
+        print("{:>8} with  5 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
         # Loop another 5 times to get to 10
-        for x in xrange(0, 5):
+        for x in range(0, 5):
             digest = sha_func(digest).digest()
-        print('{} with 10 Rounds: {}'.format(sha_type, digest.encode('hex')))
+        print("{:>8} with 10 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
     # SHA3 is handled differently as it's not in hashlib.
     # Use the SHA3 creators' version
     hash3_funcs = [
-        ('SHA3-224', sha3.SHA3_224),
-        ('SHA3-256', sha3.SHA3_256),
-        ('SHA3-384', sha3.SHA3_384),
-        ('SHA3-512', sha3.SHA3_512)]
+        ("SHA3-224", sha3.SHA3_224),
+        ("SHA3-256", sha3.SHA3_256),
+        ("SHA3-384", sha3.SHA3_384),
+        ("SHA3-512", sha3.SHA3_512),
+    ]
 
     for (sha_type, sha_func) in hash3_funcs:
-        digest = sha_func([ord(c) for c in 'abc'])
+        digest = sha_func([ord(c) for c in "abc"])
 
         # Only loop 4 times since an iteration was done above
-        for x in xrange(0, 4):
+        for x in range(0, 4):
             digest = sha_func(digest)
-        print('{} with 5 Rounds: {}'.format(sha_type, binascii.hexlify(digest)))
+        print("{:>8} with  5 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
         # Loop another 5 times to get to 10
-        for x in xrange(0, 5):
+        for x in range(0, 5):
             digest = sha_func(digest)
-        print('{} with 10 Rounds: {}'.format(sha_type, binascii.hexlify(digest)))
+        print("{:>8} with 10 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
-    hash3_funcs = [
-        ('SHAKE128', sha3.SHAKE128),
-        ('SHAKE256', sha3.SHAKE256)]
+    hash3_funcs = [("SHAKE128", sha3.SHAKE128), ("SHAKE256", sha3.SHAKE256)]
 
     for (sha_type, sha_func) in hash3_funcs:
-        digest = sha_func([ord(c) for c in 'abc'], 31)
+        digest = sha_func([ord(c) for c in "abc"], 31)
 
         # Only loop 4 times since an iteration was done above
-        for x in xrange(0, 4):
+        for x in range(0, 4):
             digest = sha_func(digest, 31)
-        print('{} with 5 Rounds: {}'.format(sha_type, binascii.hexlify(digest)))
+        print("{:>8} with  5 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
         # Loop another 5 times to get to 10
-        for x in xrange(0, 5):
+        for x in range(0, 5):
             digest = sha_func(digest, 31)
-        print('{} with 10 Rounds: {}'.format(sha_type, binascii.hexlify(digest)))
+        print("{:>8} with 10 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
-if ('__main__' == __name__):
+
+if "__main__" == __name__:
     main()
--- test/genShake.py
+++ test/genShake.py
@@ -1,42 +1,51 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import binascii
 
 import sha3
 
+
 def main():
     test_vectors = {
-        'Short': 'abc',
-        'Medium': 'abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu',
-        'Long': 'a' * 1000000
+        "Short": "abc",
+        "Medium": "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+        "Long": "a" * 1000000,
     }
-    
-    hash_funcs = {'SHAKE128': sha3.SHAKE128, 'SHAKE256': sha3.SHAKE256}    
+
+    hash_funcs = {"SHAKE128": sha3.SHAKE128, "SHAKE256": sha3.SHAKE256}
     output_lens = [31, 63]
-    
+
     for hash_name, hash_func in hash_funcs.items():
         for vector_name, vector_value in test_vectors.items():
             for output_len in output_lens:
-                print('%s with %s UTF-8 Input and %d bit Output: %s' % (
-                    hash_name,
-                    vector_name,
-                    output_len * 8,
-                    binascii.hexlify(hash_func(vector_value.encode(), output_len)).decode()
+                print(
+                    "{} with {:>6}    UTF-8 Input and {} bit Output: {}".format(
+                        hash_name,
+                        vector_name,
+                        output_len * 8,
+                        binascii.hexlify(hash_func(vector_value.encode(), output_len)).decode(),
                     )
                 )
-                print('%s with %s UTF-16BE Input and %d bit Output: %s' % (
-                    hash_name,
-                    vector_name,
-                    output_len * 8,
-                    binascii.hexlify(hash_func(vector_value.encode('UTF-16BE'), output_len)).decode()
+                print(
+                    "{} with {:>6} UTF-16BE Input and {} bit Output: {}".format(
+                        hash_name,
+                        vector_name,
+                        output_len * 8,
+                        binascii.hexlify(
+                            hash_func(vector_value.encode("UTF-16BE"), output_len)
+                        ).decode(),
                     )
                 )
-                print('%s with %s UTF-16LE Input and %d bit Output: %s' % (
-                    hash_name,
-                    vector_name,
-                    output_len * 8,
-                    binascii.hexlify(hash_func(vector_value.encode('UTF-16LE'), output_len)).decode()
+                print(
+                    "{} with {:>6} UTF-16LE Input and {} bit Output: {}".format(
+                        hash_name,
+                        vector_name,
+                        output_len * 8,
+                        binascii.hexlify(
+                            hash_func(vector_value.encode("UTF-16LE"), output_len)
+                        ).decode(),
                     )
                 )
 
-if ('__main__' == __name__):
+
+if "__main__" == __name__:
     main()
