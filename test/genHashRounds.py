ef32c2b433fef7ad0478970aad5ae13bca5e4b18
Brian Turek <brian.turek@gmail.com>
2020-03-26 08:49:46 +0000

    Convert Python files to Python 3

--- test/genHashRounds.py
+++ test/genHashRounds.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+import binascii
 import hashlib
+
 
 def main():
     hash_funcs = [
-        ('SHA-1', hashlib.sha1),
-        ('SHA-224', hashlib.sha224),
-        ('SHA-256', hashlib.sha256),
-        ('SHA-384', hashlib.sha384),
-        ('SHA-512', hashlib.sha512)]
+        ("SHA-1", hashlib.sha1),
+        ("SHA-224", hashlib.sha224),
+        ("SHA-256", hashlib.sha256),
+        ("SHA-384", hashlib.sha384),
+        ("SHA-512", hashlib.sha512),
+    ]
 
     for (sha_type, sha_func) in hash_funcs:
-        digest = sha_func('abc').digest()
+        digest = sha_func("abc".encode()).digest()
 
         # Only loop 4 times since an iteration was done above
-        for x in xrange(0, 4):
+        for x in range(0, 4):
             digest = sha_func(digest).digest()
-        print('{} with 5 Rounds: {}'.format(sha_type, digest.encode('hex')))
+        print("{:>7} with  5 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
         # Loop another 5 times to get to 10
-        for x in xrange(0, 5):
+        for x in range(0, 5):
             digest = sha_func(digest).digest()
-        print('{} with 10 Rounds: {}'.format(sha_type, digest.encode('hex')))
+        print("{:>7} with 10 Rounds: {}".format(sha_type, binascii.hexlify(digest).decode()))
 
-if ('__main__' == __name__):
+
+if "__main__" == __name__:
     main()
