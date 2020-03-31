ff9fb50f30f84d2da7842f2193ec0fab5b24e44f
Brian Turek <brian.turek@gmail.com>
2020-03-31 07:35:12 +0100

    Remove unneeded old build files

--- build/externs.js
+++
@@ -1,5 +0,0 @@
-/* This file is needed so that the Google Closure Compiler does not complain
- * about the module, define, and exports variables being undefined */
-var module = null;
-var define = null;
-var exports = null;
