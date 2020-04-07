b16cf3b3bf73adf1d169575dea5ce2b2bb1e562c
Brian Turek <brian.turek@gmail.com>
2020-04-07 06:07:01 +0100

    Disable eslint TS rule against JS/MJS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.js", "*.mjs"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
   ],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
     "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/class-name-casing": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
-  }
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
 };
