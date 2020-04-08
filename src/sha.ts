609796db96b0c77e1415031c3da64b3b9fa80f81
Brian Turek <brian.turek@gmail.com>
2020-04-08 14:55:31 +0100

    Setting internal object to private significantly cleans up declaration files

--- src/sha.ts
+++ src/sha.ts
@@ -24,7 +24,7 @@ type VariantType =
   | "SHAKE256";
 
 export default class jsSHA {
-  protected readonly shaObj: jsSHA1 | jsSHA256 | jsSHA512 | jsSHA3;
+  private readonly shaObj: jsSHA1 | jsSHA256 | jsSHA512 | jsSHA3;
   constructor(variant: VariantType, inputFormat: "TEXT", options?: InputOptionsEncodingType);
   constructor(variant: VariantType, inputFormat: FormatNoTextType, options?: InputOptionsNoEncodingType);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
