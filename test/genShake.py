504a8c857f076b5a375fc97efc8553c38ef315ba
Brian Turek <brian.turek@gmail.com>
2020-04-08 07:26:03 +0100

    Add test cases that break SHAKE

--- test/genShake.py
+++ test/genShake.py
@@ -11,38 +11,18 @@ def main():
     }
 
     hash_funcs = {"SHAKE128": hashlib.shake_128, "SHAKE256": hashlib.shake_256}
-    output_lens = [31, 63]
+    output_lens = [31, 63, 256]
 
     for hash_name, hash_func in hash_funcs.items():
         for vector_name, vector_value in test_vectors.items():
             for output_len in output_lens:
                 print(
-                    "{} with {:>6}    UTF-8 Input and {} bit Output: {}".format(
+                    "{} with {:>6}    Input and {:>6} bit Output: {}".format(
                         hash_name,
                         vector_name,
                         output_len * 8,
                         binascii.hexlify(
                             hash_func(vector_value.encode()).digest(output_len)
-                        ).decode(),
-                    )
-                )
-                print(
-                    "{} with {:>6} UTF-16BE Input and {} bit Output: {}".format(
-                        hash_name,
-                        vector_name,
-                        output_len * 8,
-                        binascii.hexlify(
-                            hash_func(vector_value.encode("UTF-16BE")).digest(output_len)
-                        ).decode(),
-                    )
-                )
-                print(
-                    "{} with {:>6} UTF-16LE Input and {} bit Output: {}".format(
-                        hash_name,
-                        vector_name,
-                        output_len * 8,
-                        binascii.hexlify(
-                            hash_func(vector_value.encode("UTF-16LE")).digest(output_len)
                         ).decode(),
                     )
                 )
